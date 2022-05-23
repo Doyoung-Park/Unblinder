ca04d3627b23fc545a0b9cb0748378c680089b69
Doyoung Park <doodoo55@gachon.ac.kr>
2022-05-23 10:27:59 +0900

    .

--- Mobile_TermProject2/app/src/main/java/Stores/GettingStores.java
+++ Mobile_TermProject2/app/src/main/java/Stores/GettingStores.java
@@ -1,8 +1,22 @@
 package Stores;
 
+import android.content.Context;
+import android.os.Environment;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
+
+
 public class GettingStores {
-    ArrayList<Store> stores = new ArrayList<Store>();
+
 
 }
--- Mobile_TermProject2/app/src/main/java/com/android/example/mobile_termproject2/Address.java
+++ Mobile_TermProject2/app/src/main/java/com/android/example/mobile_termproject2/Address.java
@@ -2,10 +2,14 @@ package com.android.example.mobile_termproject2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 
 public class Address extends AppCompatActivity {
@@ -16,14 +20,25 @@ public class Address extends AppCompatActivity {
         setContentView(R.layout.input_address);
 
         Button chooseButton = (Button) findViewById(R.id.choose);
+        TextInputEditText addressText = (TextInputEditText)findViewById(R.id.inputAddress);
 
         chooseButton.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View view) {
+                // 사용자가 입력한 주소가 userAddress 로 저장됨.
+                String userAddress = addressText.getText().toString();
+
+                System.out.println(userAddress);    // test 용
+
+                // userAddress 라는 이름으로 intent 에 담음.
                 Intent intent = new Intent(getApplicationContext(), FoodName.class);
-                startActivity(intent);
+                intent.putExtra("userAddress",userAddress);
+
+                startActivity(intent);  // foodName 액티비티 열어둠 (단방향)
             }
         });
+
+
     }
 }
